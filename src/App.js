1c9d36955d9d3a7b74b12b5217d8ee9b24adf6f0
Lamont Taylor <mlamont8@hotmail.com>
2020-10-25 09:26:11 -0400

    added purge

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+    <div className="max-w-md mx-auto flex p-6 bg-blue-100 mt-10 rounded-lg shadow-xl">
     <div className="ml-6 pt-1">
       <h1 className="text-2xl text-blue-700 leading-tight">
         Make My Day
